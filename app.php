3f17aa0d45565778447c56bf7b9fdd74491dbf50
Aryan Raval <2533aryan@gmail.com>
2023-02-01 21:40:40 -0500

    php added

---
+++ app.php
@@ -0,0 +1,39 @@
+<style>
+  h1 {
+    text-align: center;
+    font-size: 36px;
+    font-family: sans-serif;
+    margin-top: 50px;
+  }
+
+  form {
+    width: 500px;
+    margin: 0 auto;
+    text-align: center;
+    margin-top: 50px;
+  }
+
+  input[type="text"] {
+    width: 100%;
+    padding: 10px;
+    font-size: 18px;
+    margin-bottom: 20px;
+    border: 1px solid gray;
+    border-radius: 5px;
+  }
+
+  input[type="submit"] {
+    width: 200px;
+    padding: 10px;
+    font-size: 18px;
+    background-color: #2196F3;
+    color: white;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
+
+  input[type="submit"]:hover {
+    background-color: #1E88E5;
+  }
+</style>
