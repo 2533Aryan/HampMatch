e855d7c0b6d998960ec2f19b2dacfef2203baeda
Aryan Raval <2533aryan@gmail.com>
2023-02-06 20:48:19 -0500

    js edited

--- professor-match.js
+++ professor-match.js
@@ -57,3 +57,35 @@ async function findMatch(interests) {
   
   return matchName;
 }
+
+// async function findMatch(interests) {
+//   let response = await fetch("professors.csv");
+//   let data = await response.text();
+//   let professors = [];
+//   let rows = data.split("\n");
+//   for (let i = 1; i < rows.length; i++) {
+//     let cells = rows[i].split(",");
+//     professors.push({
+//       name: cells[0],
+//       interests: cells[1].split(";")
+//     });
+//   }
+  
+//   let matchScore = 0;
+//   let matchName = "";
+  
+//   for (let i = 0; i < professors.length; i++) {
+//     let score = 0;
+//     for (let j = 0; j < interests.length; j++) {
+//       if (professors[i].interests.includes(interests[j])) {
+//         score++;
+//       }
+//     }
+//     if (score > matchScore) {
+//       matchScore = score;
+//       matchName = professors[i].name;
+//     }
+//   }
+  
+//   return matchName;
+// }
