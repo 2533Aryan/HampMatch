847bdf17aabbdbce4535498f97a29e8a8db967f1
Aryan Raval <2533aryan@gmail.com>
2023-02-08 17:58:00 -0500

    experiment

--- professor-match.js
+++ professor-match.js
@@ -1,33 +1,33 @@
 document.getElementById("match-form").addEventListener("submit", function(event){
-  // event.preventDefault();
-  // let name = "John"
-  // alert("Your best match is Professor " + name);
+  event.preventDefault();
+  let name = "John"
+  alert("Your best match is Professor " + name);
   
   // display();
 
-  // // Get the interests entered by the user
-  // let interest1 = document.querySelector('input[name="interest1"]').value;
-  // let interest2 = document.querySelector('input[name="interest2"]').value;
-  // let interest3 = document.querySelector('input[name="interest3"]').value;
+  // Get the interests entered by the user
+  let interest1 = document.querySelector('input[name="interest1"]').value;
+  let interest2 = document.querySelector('input[name="interest2"]').value;
+  let interest3 = document.querySelector('input[name="interest3"]').value;
 
-  // // Find the best match professor
-  // let bestMatch = matchInterests(interest1, interest2, interest3);
+  // Find the best match professor
+  let bestMatch = matchInterests(interest1, interest2, interest3);
 
-  // // Display the result to the user
-  // alert("The best match professor is " + bestMatch.Name + " (" + bestMatch.Title + ") with an area of study in " + bestMatch.Area_of_Study + ".");
+  // Display the result to the user
+  alert("The best match professor is " + bestMatch.Name + " (" + bestMatch.Title + ") with an area of study in " + bestMatch.Area_of_Study + ".");
 
 });
 
 
-function display() {
-  let interests = [];
-  interests.push(document.querySelector("input[name='interest1']").value);
-  interests.push(document.querySelector("input[name='interest2']").value);
-  interests.push(document.querySelector("input[name='interest3']").value);
+// function display() {
+//   let interests = [];
+//   interests.push(document.querySelector("input[name='interest1']").value);
+//   interests.push(document.querySelector("input[name='interest2']").value);
+//   interests.push(document.querySelector("input[name='interest3']").value);
   
-  let match = findMatch(interests);
-  alert("Your best match is Professor " + match);
-}
+//   let match = findMatch(interests);
+//   alert("Your best match is Professor " + match);
+// }
 
 // async function display() {
 //   let form = document.getElementById("match-form");
