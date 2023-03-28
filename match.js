6b7515e54ab1881472961f25d860a87068f224ce
Aryan Raval <2533aryan@gmail.com>
2023-03-28 12:05:17 -0400

    js connect json

--- match.js
+++ match.js
@@ -1,7 +1,29 @@
-const holder = document.querySelector("button")
+// const holder = document.querySelector("button")
 
-function pageTwo() {
+// function pageTwo() {
     
-}
+// }
 
-addEventListener('click', pageTwo)
+// addEventListener('click', pageTwo)
+
+const url = "professors.json";
+
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    const professorsList = document.getElementById("professors-list");
+    let professorsHtml = "<ul>";
+    data.professors.forEach(professor => {
+      const professorHtml = `
+        <li>
+          <h2>${professor.first_name} ${professor.last_name}</h2>
+          <p><strong>Area of Study:</strong> ${professor.area_of_study_1}, ${professor.area_of_study_2}, ${professor.area_of_study_3}</p>
+          <p><strong>Email:</strong> ${professor.email_id}</p>
+        </li>
+      `;
+      professorsHtml += professorHtml;
+    });
+    professorsHtml += "</ul>";
+    professorsList.innerHTML = professorsHtml;
+  })
+  .catch(error => console.error(error));
