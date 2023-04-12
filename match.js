8c0bc13a5ee15115791b64a42ba50b95e195c3c9
Aryan Raval <2533aryan@gmail.com>
2023-04-12 02:15:47 -0400

    progress

--- match.js
+++ match.js
@@ -1,4 +1,73 @@
+const matchButton = document.querySelector('#match-button');
+const keywordsInput = document.querySelector('input[name="Area1"]');
+const locationInput = document.querySelector('input[name="Area2"]');
+const area3Input = document.querySelector('input[name="Area3"]');
+const featuredJobsContainer = document.querySelector('.featured-jobs');
+//         area1Input : document.querySelector('input[name="Area1"]'),
+//         area2Input : document.querySelector('input[name="Area2"]'),
+//         area3Input : document.querySelector('input[name="Area3"]')
 
+const url = "professor.json";
+
+// fetch(url)
+//   .then(response => response.json())
+//   .then(jobs => {
+//     jobs.forEach(generateJobElement);
+//   });
+
+matchButton.addEventListener('click', event => {
+    event.preventDefault();
+
+    const keywords = keywordsInput.value.trim().toLowerCase();
+    const location = locationInput.value.trim().toLowerCase();
+
+    // Clear the featured jobs container
+    featuredJobsContainer.innerHTML = '';
+
+    fetch('jobs-data.json')
+    .then(response => response.json())
+    .then(jobs => {
+        // Filter the jobs by keywords and location
+        const filteredJobs = jobs.filter(job => {
+        return (
+            job.title.toLowerCase().includes(keywords) &&
+            job.location.toLowerCase().includes(location)
+        );
+        });
+
+        // Generate and append the elements for the filtered jobs
+        filteredJobs.forEach(generateJobElement);
+});
+});
+
+function generateJobElement(job) {
+    // Create the elements for the job
+    const jobElement = document.createElement('div');
+    const titleElement = document.createElement('h3');
+    const companyElement = document.createElement('p');
+    const locationElement = document.createElement('p');
+    const descriptionElement = document.createElement('p');
+    const applyButton = document.createElement('a');
+  
+    // Set the content and attributes of the elements
+    titleElement.textContent = job.title;
+    companyElement.textContent = job.company;
+    locationElement.textContent = job.location;
+    descriptionElement.textContent = job.description;
+    applyButton.textContent = 'Apply Now';
+    applyButton.href = job.apply_url;
+  
+    // Append the elements to the job element
+    jobElement.appendChild(titleElement);
+    jobElement.appendChild(companyElement);
+    jobElement.appendChild(locationElement);
+    jobElement.appendChild(descriptionElement);
+    jobElement.appendChild(applyButton);
+  
+    // Append the job element to the featured jobs container
+    featuredJobsContainer.appendChild(jobElement);
+  }
+  
 // const url = "professor.json";
 
 // fetch(url)
