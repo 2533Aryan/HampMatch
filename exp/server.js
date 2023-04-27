b8737fab5823fbb82848589473483a9964bb3db5
Aryan Raval <2533aryan@gmail.com>
2023-04-27 03:39:38 -0400

    new

---
+++ exp/server.js
@@ -0,0 +1,22 @@
+const http = require("http");
+const fs = require("fs");
+
+http.createServer((req, res) => {
+  if (req.method === "POST" && req.url === "/submit-data") {
+    let jsonData = "";
+    req.on("data", chunk => {
+      jsonData += chunk;
+    });
+    req.on("end", () => {
+      const data = JSON.parse(jsonData);
+      fs.writeFile("data.json", jsonData, err => {
+        if (err) throw err;
+        res.writeHead(200, {"Content-Type": "text/plain"});
+        res.end("Data written to file.");
+      });
+    });
+  } else {
+    res.writeHead(404, {"Content-Type": "text/plain"});
+    res.end("Page not found.");
+  }
+}).listen(3000);
